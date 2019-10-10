fef228a2dc7b0672c6944a20ac708616cfffe2ef
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-10 19:00:18 +0530

    run method over ridden

--- src/story/RacingRabbit.java
+++ src/story/RacingRabbit.java
@@ -1,8 +1,30 @@
 package story;
 
-public class RacingRabbit {
+public class RacingRabbit extends Thread {
     public RacingRabbit(String name) {
         super(name);
     }
 
+    @Override
+    public void run() {
+        System.out.print(getName() + " has started");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.print("interrupt!!!");
+        }
+        System.out.print(getName() + " has reached the oak tree");
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            System.out.print("interrupt!!!");
+        }
+        System.out.print(getName() + " has reached the hills");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.print("interrupt!!!");
+        }
+        System.out.print(getName() + " finished the race.....");
+    }
 }
