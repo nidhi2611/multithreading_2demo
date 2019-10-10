5834923ed44a30b2ae43418904f6bdd2d1a1fd52
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-10 19:19:24 +0530

    thread name given

--- src/story/Race.java
+++ src/story/Race.java
@@ -2,6 +2,8 @@ package story;
 
 public class Race {
     public static void main(String[] args) {
+        RacingTortoise tor = new RacingTortoise("Tammy");
+        RacingRabbit rab = new RacingRabbit("Rabby");
 
     }
 }
