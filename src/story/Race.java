7be29c8008809f7e83fdcdcc1ba317cb2a59f871
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2019-10-10 19:19:57 +0530

    tortoise main called

--- src/story/Race.java
+++ src/story/Race.java
@@ -4,6 +4,7 @@ public class Race {
     public static void main(String[] args) {
         RacingTortoise tor = new RacingTortoise("Tammy");
         RacingRabbit rab = new RacingRabbit("Rabby");
+        tor.start();
 
     }
 }
